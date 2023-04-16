be882806f20873630333904d2c292d3e1ca0509d
piotrsrednickiDS360 <piotrsrednicki@ds360.pl>
2023-04-16 11:19:10 +0200

    creating default project structure

---
+++ main.py
@@ -0,0 +1,17 @@
+from tkinter import *
+from PIL import ImageTk, Image
+
+# label, button, entry,
+# grid, column, row, command (lambda do parametrów, text, bg, fg, pierwszy parametr oznacza gdzie coś przynalezy,
+# pack pakuje, get,
+# insert dodaje default text, columnspan, rowspan, height, padx, pady
+# width, borderwidth, insert, delete, root.quit wychodzi z programu
+
+root = Tk()
+root.title("Restaurant Manager")
+root.iconbitmap("Resources/restaurant_4373.ico")
+
+
+
+root.mainloop()
+
