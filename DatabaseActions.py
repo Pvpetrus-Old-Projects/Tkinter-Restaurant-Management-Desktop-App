2bf8c431d3abff695fa75114d430bfae4392ddf0
piotrsrednickiDS360 <piotrsrednicki@ds360.pl>
2023-04-24 22:50:21 +0200

    filling up database

--- DatabaseActions.py
+++ DatabaseActions.py
@@ -6,12 +6,13 @@ from sqlite3 import Connection
 def SetupDatabase() -> None:
     SQLConnection: Connection = sqlite3.connect('RestaurantDatabase')
     coursor = SQLConnection.cursor()
+    # Deleting tables
     coursor.execute("""DROP TABLE IF EXISTS user""")
     coursor.execute("""DROP TABLE IF EXISTS orderTable""")
     coursor.execute("""DROP TABLE IF EXISTS item""")
+    # Creating tables
     coursor.execute("""CREATE TABLE IF NOT EXISTS user (id integer PRIMARY KEY, username text NOT NULL UNIQUE, password text NOT 
     NULL)""")
-
     coursor.execute("""CREATE TABLE IF NOT EXISTS orderTable (id integer PRIMARY KEY, userId integer, 
                         orderType text, location text, distance real,
                         customerName text, orderStatus text, createAt datetime, acceptAt datetime, 
@@ -22,19 +23,35 @@ def SetupDatabase() -> None:
                         description text, mainPrice real, 
                         secondaryPrice real, count integer, FOREIGN KEY (orderId)
                         REFERENCES orderTable (id) ON DELETE CASCADE)""")
-
+    # Filling the database with data
+    # Creating users
     coursor.execute("""INSERT INTO user(id, username, password) 
                         VALUES (1,'admin','admin')""")
-
+    # Creating orders
     coursor.execute("""INSERT INTO orderTable(id, userId, orderType, location, distance, customerName, orderStatus, 
                         createAt, acceptAt, finishAt, estimate) 
                         VALUES (1, 1, 'takeout', '', 0, 'Martin', 'incoming', '2012-06-18T10:34:09', NULL, NULL, NULL)""")
-
+    coursor.execute("""INSERT INTO orderTable(id, userId, orderType, location, distance, customerName, orderStatus, 
+                            createAt, acceptAt, finishAt, estimate) 
+                            VALUES (2, 1, 'takeout', '', 0, 'Martin', 'accepted', '2012-06-18T11:34:09', 
+                            '2012-06-18T11:37:09', NULL, NULL)""")
+    coursor.execute("""INSERT INTO orderTable(id, userId, orderType, location, distance, customerName, orderStatus, 
+                            createAt, acceptAt, finishAt, estimate) 
+                            VALUES (3, 1, 'takeout', '', 5, 'Martin', 'ready', '2012-06-18T12:34:09', 
+                            '2012-06-18T12:34:09', '2012-06-18T12:36:09', '2012-06-18T12:39:09')""")
+    # Creating items
     coursor.execute("""INSERT INTO item(id, orderId, name, description, mainPrice, secondaryPrice, count) 
                         VALUES (1,1,'Hot-dog','Additional ketchup + additional cheese', 10.5, 2.5, 1)""")
-
     coursor.execute("""INSERT INTO item(id, orderId, name, description, mainPrice, secondaryPrice, count) 
                             VALUES (2,1,'Hamburger','Additional mayo', 15.5, 1.0, 2)""")
+    coursor.execute("""INSERT INTO item(id, orderId, name, description, mainPrice, secondaryPrice, count) 
+                            VALUES (3,2,'Hot-dog','Additional ketchup ', 10.5, 2.5, 1)""")
+    coursor.execute("""INSERT INTO item(id, orderId, name, description, mainPrice, secondaryPrice, count) 
+                                VALUES (4,2,'Pastrami','Additional mustard', 15.5, 1.0, 2)""")
+    coursor.execute("""INSERT INTO item(id, orderId, name, description, mainPrice, secondaryPrice, count) 
+                            VALUES (5,2,'Fish and chips','Additional ketchup + additional cheese', 10.5, 2.5, 1)""")
+    coursor.execute("""INSERT INTO item(id, orderId, name, description, mainPrice, secondaryPrice, count) 
+                                VALUES (6,3,'Chips','Additional ketchup', 15.5, 1.0, 2)""")
     # """Update SqliteDb_developers set salary = 10000 where id = 4"""
     # """DELETE from SqliteDb_developers where id = 4"""
 
@@ -186,10 +203,10 @@ def returnTotalRevenueForUser(user_id) -> float:
     readyOrders: list = coursor.fetchall()
     sum: float = 0
     for order in readyOrders:
-        coursor.execute('SELECT * FROM item where orderId=?', (user_id,))
+        coursor.execute('SELECT * FROM item where orderId=?', (order[0],))
         orderItems: list = coursor.fetchall()
         for item in orderItems:
-            sum += (item[4] + item[5])*item[6]
+            sum += (item[4] + item[5]) * item[6]
     SQLConnection.close()
     return sum
 
@@ -205,3 +222,53 @@ def returnAcceptedOrderRatioForUser(user_id) -> float:
             sumOfAcceptedOrders += 1
     SQLConnection.close()
     return sumOfAcceptedOrders / len(orders) * 100
+
+
+def returnLastSevenDaysProfitList(user_id) -> list:
+    SQLConnection: Connection = sqlite3.connect('RestaurantDatabase')
+    coursor = SQLConnection.cursor()
+    coursor.execute('SELECT * FROM orderTable where userId=?', (user_id,))
+    orders: list = coursor.fetchall()
+    return []
+
+
+def returnTopSellingDishesList(user_id) -> list:
+    SQLConnection: Connection = sqlite3.connect('RestaurantDatabase')
+    coursor = SQLConnection.cursor()
+    coursor.execute('SELECT * FROM orderTable where userId=?', (user_id,))
+    orders: list = coursor.fetchall()
+    items: list = []
+    for order in orders:
+        coursor.execute('SELECT * FROM item where orderId=?', (order[0],))
+        buforItems = coursor.fetchall()
+        for item in buforItems:
+            items.append(item)
+    itemsWithCountsTupleList: list[tuple] = []
+    # counting items by name
+    for item in items:
+        foundAnother = False
+        if len(itemsWithCountsTupleList) == 0:
+            itemsWithCountsTupleList.append((item[2], 1))
+        for index, anotherItem in enumerate(itemsWithCountsTupleList):
+            if anotherItem[0] == item[2]:
+                itemsWithCountsTupleList[index] = anotherItem[0], anotherItem[1] + 1
+                foundAnother = True
+                break
+        if not foundAnother:
+            itemsWithCountsTupleList.append((item[2], 1))
+
+    # sorting list of tuples
+    lst = len(itemsWithCountsTupleList)
+    for i in range(0, lst):
+        for j in range(0, lst - i - 1):
+            if itemsWithCountsTupleList[j][1] > itemsWithCountsTupleList[j + 1][1]:
+                temp = itemsWithCountsTupleList[j]
+                itemsWithCountsTupleList[j] = itemsWithCountsTupleList[j + 1]
+                itemsWithCountsTupleList[j + 1] = temp
+
+    itemsWithCountsTupleList.reverse()
+    if len(itemsWithCountsTupleList) < 10:
+        return itemsWithCountsTupleList
+    else:
+        return itemsWithCountsTupleList[:10]
+    SQLConnection.close()
